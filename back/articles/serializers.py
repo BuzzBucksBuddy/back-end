6ee8c0378bae5d867a4932e0f2cc191166fdea56
junsukim <julsukim96@gmail.com>
2023-11-17 16:48:06 +0900

    junsu-back

--- back/articles/serializers.py
+++ back/articles/serializers.py
@@ -1,17 +1,71 @@
 from rest_framework import serializers
 from .models import Article, Comment, ArticleBankCategory, ArticleProductCategory
+from django.contrib.auth import get_user_model
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ArticleListSerializer(serializers.ModelSerializer):
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('username',)
+
+    class ArticleProductCategorySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = ArticleProductCategory
+            fields = ('name',)
+    
+    class ArticleBankCategorySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = ArticleBankCategory
+            fields = ('name',)
+    
+    user = UserSerializer(read_only=True)
+    article_product_category = ArticleProductCategorySerializer(read_only=True)
+    article_bank_category = ArticleBankCategorySerializer(read_only=True)
+
     class Meta:
         model = Article
         fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('username',)
+    
+    user = UserSerializer(read_only=True)
+    
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ('article',)
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('username',)
+
+    class ArticleProductCategorySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = ArticleProductCategory
+            fields = ('name',)
+    
+    class ArticleBankCategorySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = ArticleBankCategory
+            fields = ('name',)
+    
+    user = UserSerializer(read_only=True)
+    article_product_category = ArticleProductCategorySerializer(read_only=True)
+    article_bank_category = ArticleBankCategorySerializer(read_only=True)
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+        read_only_fields = ('like_users', 'user',)
 
 
 class ArticleBankCategorySerializer(serializers.ModelSerializer):
--- back/articles/urls.py
+++ back/articles/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    
+    path('', views.article_list),
+    path('<int:article_pk>/', views.article_detail),
+    path('<int:article_pk>/comments/', views.comment_list),
+    path('category/products/', views.product_category_list),
+    path('category/banks/', views.bank_category_list),
 ]
