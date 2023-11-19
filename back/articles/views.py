02ae895c6094c3e4a5fb0f5b2009025045523d43
julsukim <julsukim96@gmail.com>
2023-11-20 02:53:03 +0900

    게시판 수정 등등

--- back/articles/views.py
+++ back/articles/views.py
@@ -11,16 +11,18 @@ from .serializers import CommentSerializer, ArticleListSerializer, ArticleSerial
 from .models import Article, Comment, ArticleBankCategory, ArticleProductCategory
 from products.models import SavingProducts, DepositProducts
 
+# 로그인 필요 =>
+# 게시글 작성, 수정, 삭제
+# 코멘트 작성, 수정, 삭제
+# 게시글 좋아요
+# 코멘트 좋아요
 
-@api_view(['GET', 'POST'])
-# @permission_classes([IsAuthenticated])
-def article_list(request):
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleListSerializer(articles, many=True)
-        return Response(serializer.data)
 
-    elif request.method == 'POST':
+# 게시글 CREATE
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def article_create(request):
+    if request.method == 'POST':
         article_bank_category = ArticleBankCategory.objects.get(pk=request.data['article_bank_category'])
         article_product_category = ArticleProductCategory.objects.get(pk=request.data['article_product_category'])
         serializer = ArticleSerializer(data=request.data)
@@ -30,15 +32,32 @@ def article_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'DELETE', 'PUT'])
+# 게시글 READ ALL
+@api_view(['GET'])
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+
+
+# 게시글 READ
+@api_view(['GET'])
 def article_detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+
+
+# 게시글 DELETE, UPDATE
+@api_view(['DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
+def article_control(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
     
-    elif request.method == 'DELETE':
+    if request.method == 'DELETE':
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
@@ -49,6 +68,7 @@ def article_detail(request, article_pk):
             return Response(serializer.data)
 
 
+# 카테고리(예금, 적금 등등) READ
 @api_view(['GET', 'POST'])
 def product_category_list(request):
     if request.method == 'GET':
@@ -57,6 +77,7 @@ def product_category_list(request):
         return Response(serializer.data)
 
 
+# 카테고리(은행) READ & (CREATE, UPDATE)
 @api_view(['GET', 'POST'])
 def bank_category_list(request):
     if request.method == 'GET':
@@ -88,9 +109,10 @@ def bank_category_list(request):
             return Response({ 'message': 'saved'})
         else:
             return Response({ 'message': 'not worked'})
-    
 
-@api_view(['GET', 'POST', 'DELETE'])
+
+# 코멘트 READ
+@api_view(['GET'])
 def comment_list(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
 
@@ -98,15 +120,23 @@ def comment_list(request, article_pk):
         comments = Comment.objects.filter(article=article)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
-    
-    elif request.method == 'POST':
+
+# 코멘트 CREATE
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def comment_create(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if request.method == 'POST':
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=request.user, article=article)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
 
+
+# 코멘트 UPDATE, DELETE
 @api_view(['PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def comment_control(request, article_pk, comment_pk):
     comment = get_object_or_404(Comment, pk=comment_pk)
 
@@ -120,11 +150,28 @@ def comment_control(request, article_pk, comment_pk):
             serializer.save()
             return Response(serializer.data)
 
-# @api_view(['GET'])
-# def bank_list(request):
-#     if request.method == 'GET':
-#         dep_banks = DepositProducts.objects.all().only('kor_co_nm')
-#         sav_banks = SavingProducts.objects.all().only('kor_co_nm')
-#         print(dep_banks)
-#         print(sav_banks)
-#         return Response(dep_banks)
+
+# 게시글 좋아요
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def article_like(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        if request.user in article.like_users.all():
+            article.like_users.remove(request.user)
+        else:
+            article.like_users.add(request.user)
+        return Response({ 'message': 'like_you?'})
+    
+
+# 코멘트 좋아요
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def comment_like(request, article_pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    if request.method == 'POST':
+        if request.user in comment.like_users.all():
+            comment.like_users.remove(request.user)
+        else:
+            comment.like_users.add(request.user)
+        return Response({ 'message': 'like_you?'})
